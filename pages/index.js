8a54ffcc6d7e87ea88491075378546ace876edb1
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-03-10 11:26:23 +0100

    Fetch wilders from backend

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,14 @@
-import Head from "next/head";
-
-import styles from "../styles/Index.module.css";
-import Logo from "../public/logo.png";
-import CounterComponent from "../components/CounterComponent";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Wilder from "../components/WilderComponent";
 
 const Home = () => {
-  const wildersData = [
-    {
-      name: "Ringo",
-      city: "London",
-      skills: [
-        { title: "JS", votes: 10 },
-        { title: "React", votes: 8 },
-      ],
-    },
-    {
-      name: "John",
-      city: "Paris",
-      skills: [
-        { title: "PHP", votes: 9 },
-        { title: "Symfony", votes: 9 },
-      ],
-    },
-    {
-      name: "George",
-      city: "Berlin",
-      skills: [
-        { title: "Ruby", votes: 10 },
-        { title: "JS", votes: 8 },
-      ],
-    },
-    {
-      name: "Paul",
-      city: "Reims",
-      skills: [
-        { title: "C++", votes: 10 },
-        { title: "Rust", votes: 8 },
-      ],
-    },
-  ];
+  const [wildersData, setWildersData] = useState([]);
+  const fetchData = async () => {
+    const result = await axios.get("http://localhost:5000/api/wilders");
+    setWildersData(result.data.result);
+  };
+  useEffect(() => fetchData(), []);
   return (
     <div>
       <header>
